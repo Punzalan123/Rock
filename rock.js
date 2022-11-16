468c7c7a337ab8ac12ef41613ee3fced77cafdd8
unknown <punzalanreiniel@gmail.com>
2022-11-16 23:42:29 +0800

    plus modal

--- rock.js
+++ rock.js
@@ -6,15 +6,16 @@ ps = document.getElementById('Pscor');
 cs = document.getElementById('Cscor');
 np = 0;
 nc = 0;
-// document.getElementById('history').style.display = 'none';
 h = document.getElementById('history');
 g = document.getElementById('score');
 i = document.getElementById('compscore');
 j = document.getElementById('playscore');
+k = document.getElementById('view');  
 h.style.display = 'none';
 g.style.display = 'none';
 i.style.display = 'none';
 j.style.display = 'none';
+k.style.display = 'none';
 pleyawan.style.display = 'none';
 compshop.style.display = 'none';
 win.style.display = 'none';
@@ -48,84 +49,59 @@ bots.forEach(button => button.addEventListener("click", () => {
     compshop.innerHTML = 'Computer: ' + comp;
     win.innerHTML = chiK();
     // his.innerHTML = hist();
+
     his = document.getElementById("disp");
-    Mhistory = document.createElement('h3');
+    Mhistory = document.createElement('h4');
     Mhistory.textContent = hist();
     his.appendChild(Mhistory);
+
+    hism = document.getElementById('disp2');
+    histmodal = document.createElement('h5');
+    histmodal.innerHTML = hist();
+    hism.appendChild(histmodal);
     
     if (his.childElementCount > 5){
       his.removeChild(his.firstElementChild);
+      k.style.display = 'block';
     }
     if (winner == true){
       np = np + 1;
       ps.innerHTML = np;
       Mhistory.style.color = 'cyan';
+      histmodal.style.color = 'cyan';
       win.style.color = 'cyan';
     } 
     if (winner == false){
       nc = nc + 1;
       cs.innerHTML = nc;
       Mhistory.style.color = '#EB4747';
+      histmodal.style.color = '#EB4747';
       win.style.color = '#EB4747';
     }
     if (winner == null){
       Mhistory.style.color = 'yellow';
+      histmodal.style.color = 'yellow';
       win.style.color = 'yellow';
     }
 }));
 
 
-function chiK(){
+const chiK = () => {
     if(playR1 == comp){
       winner = null;
       return "It's a Tie !";
     }
-
-    if (comp == "Rock" && playR1 == "Paper"){
+    if (comp == "Rock" && playR1 == "Paper" || comp == "Paper" && playR1 == "Scissors" || comp == "Scissors" && playR1 == "Rock" ){
       winner = true; 
       return "You Win !";
-    } else if(comp == "Paper" && playR1 == "Scissors"){
-      winner = true;
-      return "You Win !";
-    } else if(comp == "Scissors" && playR1 == "Rock"){
-      winner = true;
-      return "You Win !";
-    } 
-
-    if (playR1 == "Rock" && comp == "Paper") {
-      winner = false;
-      return "You Lose !";
-    } else if (playR1 == "Paper" && comp == "Scissors") {
-      winner = false;
-      return "You Lose !";
-    } else if (playR1 == "Scissors" && comp == "Rock") {
+    }else{
       winner = false;
       return "You Lose !";
     }
 }
 
-function hist(){
-  if(playR1 == "Rock" && comp == "Rock"){
-    return "Player: Rock " + " " + "Computer: Rock";
-  } else if(playR1 == "Rock" && comp == "Paper"){
-    return "Player: Rock " + " " + "Computer: Paper";
-  } else if(playR1 == "Rock" && comp == "Scissors"){
-    return "Player: Rock " + " " + "Computer: Scissors";
-  } else if(playR1 == "Scissors" && comp == "Scissors"){
-    return "Player: Scissors " + " " + "Computer: Scissors";
-  } else if(playR1 == "Scissors" && comp == "Paper"){
-    return "Player: Scissors " + " "+ "Computer: Paper";
-  } else if(playR1 == "Scissors" && comp == "Rock"){   
-    return "Player: Scissors " + " "+ "Computer: Rock";
-  } else if(playR1 == "Paper" && comp == "Paper"){
-    return "Player: Paper " + " "+ "Computer: Paper";
-  } else if(playR1 == "Paper" && comp == "Rock"){
-    return "Player: Paper " + " "+ "Computer: Rock";
-  } else if(playR1 == "Paper" && comp == "Scissors"){   
-    return "Player: Paper " + " "+ "Computer: Scissors";
-  } 
-
-  
+const hist = () =>{
+  return `Player: ${playR1}  |  Computer: ${comp}`;
 }
 
 
